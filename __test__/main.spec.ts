9b139700faf67284ae388055df8a4afff1e399bc
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-01 10:39:15 +0100

    Adding more cases for test: give as result the same number

--- __test__/main.spec.ts
+++ __test__/main.spec.ts
@@ -3,6 +3,9 @@ import { StringCalculator } from '../src/StringCalculator';
 describe('The kata string calculator should', () => {
   it('give as result the same number', () => {
     const calculator = new StringCalculator();
+
     expect(calculator.sum('1')).toBe(1);
+    expect(calculator.sum('3')).toBe(3);
+    expect(calculator.sum('10')).toBe(10);
   });
 });
