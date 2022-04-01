2c303a352cd7a91948a43c11ebb66fe18b0507c8
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-01 10:50:37 +0100

    Test success: give the sum of two numbers delimited by comma

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,6 +1,13 @@
 export class StringCalculator {
   public sum(numbersString: string): number {
     if (numbersString === '') return 0;
+    //TODO: REFACTOR change order of comprobations
+    if (numbersString.includes(',')) {
+      const numbersArray = numbersString.split(',');
+      return (
+        Number.parseInt(numbersArray[0]) + Number.parseInt(numbersArray[1])
+      );
+    }
     return Number.parseInt(numbersString);
   }
 }
